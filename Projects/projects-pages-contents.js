be8ccb5c8b5e55e59d063afc18622e783ba959fa
HoangHieuDang <hoanghieu.dang@gmail.com>
2025-06-25 00:27:17 +0200

    adding more engineering projects into the portfolio

--- Projects/projects-pages-contents.js
+++ Projects/projects-pages-contents.js
@@ -1,34 +1,94 @@
 export const engineeringProjectHTML = `
 <h2>Engineering Project</h2>
- <!--********************************************************** -->
+    <!--********************************************************** -->
     <div class="subpage-entry-div va-sub-entry-div">
         <h3><b>Generic MIDI hand glove</b></h3>
         <p>Uni project, Hamburg University of Applied Sciences </p>
         <p>2019</p>
+
         <img src="../Assets/Images/UI-arrow-down.png" class="ui-arrow-down-img" width=20px />
         <div class="entry-detail-div">
+            <div class="description-div">
+                <p class="description-paragraph">The MIDI Glove is a wearable music interface I developed using Dehnungsmessstreifen (strain gauges) and an Arduino microcontroller. </p>
+                <p class="description-paragraph">It translates hand and finger movements into MIDI signals, allowing the glove to act as a generic MIDI
+                    controller in any DAW, such as Ableton Live. </p>
+                <p class="description-paragraph">By mapping finger bends to MIDI parameters, users can control synthesizers, effects, and automation in real time through intuitive gestures — making the glove a tactile, expressive performance
+                    tool that bridges human motion and digital sound. </p>
+                <p class="description-paragraph">This project combines sensor-based hardware prototyping, embedded programming, and MIDI protocol implementation, with the goal of exploring new ways of interacting with music production
+                    environments.</p>
+            </div>
             <div class="project-img-div">
-                
-                <video width=50% class="project-video"
-                    alt="midi glove showcase with ableton and serum vst synth" controls muted>
-                    <source src="../Assets/Images/engineering/midi-glove-showcase.mp4" type="video/mp4">
-                </video>
-    
+
+                <video width=50% class="project-video" alt="midi glove showcase with ableton and serum vst synth" controls muted>
+                       <source src="../Assets/Images/engineering/midi-glove-showcase.mp4" type="video/mp4">
+                   </video>
+
             </div>
             <div class="artwork-img-div">
-            <div class="artwork-img-column">
-                <img src="../Assets/Images/engineering/midi-glove-first-prototype.jpg" class="artwork-img"
-                    alt="first prototype of midi-glove" width=40% />
-                <img src="../Assets/Images/engineering/midi-glove-second-prototype.jpg" class="artwork-img"
-                    alt="second prototype of midi-glove" width=40%/>
-            </div>
-            <div class="artwork-img-column">
-                <img src="../Assets/Images/engineering/midi-glove-third-prototype.jpg" class="artwork-img"
-                    alt="third prototype of midi-glove" width=40%/>
-                <img src="../Assets/Images/engineering/midi-glove-3d-print.png" class="artwork-img"
-                    alt="midi glove 3d print" width=40%/>
-            </div>
+                <div class="artwork-img-column">
+                    <img src="../Assets/Images/engineering/midi-glove-first-prototype.jpg" class="artwork-img" alt="first prototype of midi-glove" width=40% />
+                    <img src="../Assets/Images/engineering/midi-glove-second-prototype.jpg" class="artwork-img" alt="second prototype of midi-glove" width=40%/>
+                </div>
+                <div class="artwork-img-column">
+                    <img src="../Assets/Images/engineering/midi-glove-third-prototype.jpg" class="artwork-img" alt="third prototype of midi-glove" width=40%/>
+                    <img src="../Assets/Images/engineering/midi-glove-3d-print.png" class="artwork-img" alt="midi glove 3d print" width=40%/>
+                </div>
 
+            </div>
+        </div>
+        <img src="../Assets/Images/UI-arrow-up.png" class="ui-arrow-up-img" width=20px />
+    </div>
+    <!--********************************************************** -->
+    <div class="subpage-entry-div va-sub-entry-div">
+        <h3><b>SALT V2 – PCB Testsystem for Automated Production Quality Control</b></h3>
+        <p>Work related project, Grau GmbH</p>
+        <p>2022</p>
+        <img src="../Assets/Images/UI-arrow-down.png" class="ui-arrow-down-img" width=20px />
+        <div class="entry-detail-div">
+            <div class="description-div">
+                <p class="description-paragraph">As part of the development of the iconic SALT V2 lamp by Grau GmbH, I designed and implemented a dedicated PCB test system to automate and ensure the electrical functionality of every unit during production. The system includes:</p>
+                <ul class="description-ul">
+                    <li class="description-li">A fully functional testing software tailored to SALT’s hardware logic</li>
+                    <li class="description-li">A custom-built Prüfadaptersystem (Test Adapter Typ 127) integrating mechanical and electrical interfaces (using KiCAD and Eagle Fusion 360)</li>
+                    <li class="description-li">A 3D-printed fixture (designed in Fusion 360, using EAGLE and printed with Formlabs) to securely hold the PCB during testing</li>
+                </ul>
+
+                <p>This test station allows production operators to verify each SALT PCB before assembly, streamlining the QA process and reducing error margins significantly. The project involved hardware integration, test automation, and physical design,
+                    combining my skills in embedded systems, CAD, and process optimization.</p>
+            </div>
+            <div class="artwork-img-div">
+                <div class="artwork-img-column">
+                    <img src="../Assets/Images/engineering/SALT_1.jpg" class="artwork-img" alt="SALT_1" width=40% />
+                    <img src="../Assets/Images/engineering/SALT_2.jpg" class="artwork-img" alt="SALT_2" width=40%/>
+                </div>
+                <div class="artwork-img-column">
+                    <img src="../Assets/Images/engineering/SALT_3.jpeg" class="artwork-img" alt="SALT_3" width=40%/>
+                    <img src="../Assets/Images/engineering/SALT_4.png" class="artwork-img" alt="SALT_4" width=40%/>
+                </div>
+
+            </div>
+        </div>
+        <img src="../Assets/Images/UI-arrow-up.png" class="ui-arrow-up-img" width=20px />
+    </div>
+    <!--********************************************************** -->
+    <div class="subpage-entry-div va-sub-entry-div">
+        <h3><b>CANDLE – Cost-Effective PCB Test fixture for Embedded Programming</b></h3>
+        <p>Work related project, Grau GmbH</p>
+        <p>2023</p>
+
+        <img src="../Assets/Images/UI-arrow-down.png" class="ui-arrow-down-img" width=20px />
+        <div class="entry-detail-div">
+            <div class="description-div">
+                <p class="description-paragraph">For the PCB prototype of the CANDLE design lamp by Grau GmbH, I developed a precision 3D-printed Prüfadapter to enable reliable flashing and programming of the onboard microcontroller. </p>
+                <p class="description-paragraph">To reduce development costs, I replaced a traditional
+                    Reinhardt Test Fixture with a custom-built solution, designed in Fusion 360 and 3D-printed in-house. Despite the cost efficiency, the adapter maintains high precision in test needle alignment, ensuring reliable electrical contact with
+                    the programming interface on the CANDLE PCB. </p>
+                <p class="description-paragraph">This project demonstrates my ability to bridge mechanical design and embedded systems, delivering practical, scalable tools in a production-focused environment.</p>
+            </div>
+            <div class="artwork-img-div">
+                <div class="artwork-img-column">
+                    <img src="../Assets/Images/engineering/CANDLE_ADAPTER.jpeg" class="artwork-img" alt="CANDLE_ADAPTER"/>
+                </div>
             </div>
         </div>
         <img src="../Assets/Images/UI-arrow-up.png" class="ui-arrow-up-img" width=20px />
